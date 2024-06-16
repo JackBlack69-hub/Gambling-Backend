db89eaa12c76b74f70860a892c046aa60a3ad0f4
Param Shelar <shelarparam@gmail.com>
2024-06-16 17:38:53 +0530

    modifications to rename files in uploads

--- middleware/validateUploads.js
+++ middleware/validateUploads.js
@@ -1,13 +1,20 @@
 const multer = require("multer");
+const jwt = require("jsonwebtoken");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    console.log(file);
-    req.filename = `${Date.now()}-${file.originalname}`;
-    cb(null, `${Date.now()}-${file.originalname}`);
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+    const decoded = jwt.verify(token, "mySecretKey");
+
+    req.filename = `${decoded.userId}.${file.mimetype.split("/")[1]}`;
+    cb(null, `${decoded.userId}.${file.mimetype.split("/")[1]}`);
   },
 });
 
